e318fc9298b05f87fe71aa5260f1ab18f82282d8
CarlMurray <carl.j.murray@gmail.com>
2024-07-31 22:24:05 +0100

    chore: init react project and build header component

---
+++ failte/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.scss";
+// import "../../assets/css/index.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <a href="#">fáilte</a>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Play</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
