63693e4f1e24f236ee509b4278d4577d352ba088
CarlMurray <carl.j.murray@gmail.com>
2024-08-03 18:35:44 +0100

    chore: add action button icons and styles

--- failte/src/components/SearchContainer/SearchResultCard/AttractionActionButton/AttractionActionButton.jsx
+++ failte/src/components/SearchContainer/SearchResultCard/AttractionActionButton/AttractionActionButton.jsx
@@ -1,17 +1,34 @@
 import "./AttractionActionButton.scss";
+import { DirectionsIcon, PhoneIcon, LinkIcon } from "../../Icons/Icons";
+import { useState } from "react";
 
 export default function AttractionActionButton({ url, phone, directions }) {
+  const [hovered, setHovered] = useState(false);
+
   let link = null;
+  let icon = null;
   if (url) {
     link = url;
+    icon = <LinkIcon />;
   } else if (phone) {
     link = phone;
+    icon = <PhoneIcon />;
   } else if (directions) {
     link = directions;
+    icon = <DirectionsIcon />;
   }
   return (
-    <a className="attraction-action-button-circle" href={link}>
-      <img src="" />
+    <a
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={
+        hovered
+          ? "hovered attraction-action-button-circle"
+          : "attraction-action-button-circle"
+      }
+      href={link}
+    >
+      {icon}
     </a>
   );
 }
